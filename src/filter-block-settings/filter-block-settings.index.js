ca44c8a036d4ac8dba3c68ba168f0a6c19da98db
stuffnting <richard@stuffnting.com>
2023-04-22 13:50:59 +0100

    Add filter-block-setting block

---
+++ src/filter-block-settings/filter-block-settings.index.js
@@ -0,0 +1,60 @@
+/**
+ * @link https://make.wordpress.org/core/2023/02/28/custom-settings-wordpress-6-2/
+ */
+
+import { addFilter } from "@wordpress/hooks";
+import { select } from "@wordpress/data";
+
+/******************************************************************************
+ * Restrict the spacing units that can be
+ * selected in the Column block to pixels:
+ *****************************************************************************/
+
+function myprefixFilterColumnSpacingUnits(
+  settingValue,
+  settingName,
+  clientId,
+  blockName
+) {
+  if (blockName === "core/column" && settingName === "spacing.units") {
+    return ["px"];
+  }
+  return settingValue;
+}
+
+addFilter(
+  "blockEditor.useSetting.before",
+  "myprefix/filter-column-space-units",
+  myprefixFilterColumnSpacingUnits
+);
+
+/******************************************************************************
+ * Disable text color controls on Heading blocks
+ * when placed inside of Media & Text blocks.
+ *****************************************************************************/
+function myprefixNoColourHeading(
+  settingValue,
+  settingName,
+  clientId,
+  blockName
+) {
+  if (blockName === "core/heading") {
+    const { getBlockParents, getBlockName } = select("core/block-editor");
+    const blockParents = getBlockParents(clientId, true);
+    const inMediaText = blockParents.some(
+      (ancestorId) => getBlockName(ancestorId) === "core/media-text"
+    );
+
+    if (inMediaText && settingName === "color.text") {
+      return false;
+    }
+  }
+
+  return settingValue;
+}
+
+addFilter(
+  "blockEditor.useSetting.before",
+  "myPlugin/useSetting.before",
+  myprefixNoColourHeading
+);
---
+++ src/filter-block-settings/filter-block-settings.php
@@ -0,0 +1,19 @@
+<?php
+
+add_action( 'enqueue_block_editor_assets', 'myprefix_filter_block_settings' );
+
+function myprefix_filter_block_settings() {
+
+  if ( ! function_exists( 'register_block_type' ) ) {
+    // Gutenberg is not active.
+    return;
+  }
+
+  wp_enqueue_script(
+    'myprefix-filter-block-settings-script',
+    MYPREFIX_GUT_BLOCKS_PLUGIN_URL . basename( __DIR__ ) . '/index.js',
+    array(),
+    filemtime( MYPREFIX_GUT_BLOCKS_PLUGIN_PATH . basename( __DIR__ ) . '/index.js' ), // *** Dev only
+    true
+  ); 
+}
