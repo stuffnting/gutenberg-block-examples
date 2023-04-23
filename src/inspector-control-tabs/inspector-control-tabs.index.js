debd093527272993c8bea9b3adb1be3559320898
stuffnting <richard@stuffnting.com>
2023-04-23 17:11:16 +0100

    Add inspector-control-tabs block

---
+++ src/inspector-control-tabs/inspector-control-tabs.index.js
@@ -0,0 +1,28 @@
+import { registerBlockType } from "@wordpress/blocks";
+import { useBlockProps } from "@wordpress/block-editor";
+import { __ } from "@wordpress/i18n";
+
+import { TheInspectorControls } from "./inspector-controls";
+import { PostListTitle } from "./post-list-title";
+import { GetPosts } from "./get-posts";
+import metadata from "./inspector-control-tabs.block.json";
+
+registerBlockType(metadata.name, {
+  edit: (props) => {
+    const blockProps = useBlockProps();
+
+    return (
+      <>
+        <TheInspectorControls parentProps={props} />
+        <div {...blockProps}>
+          <PostListTitle parentProps={props} />
+          <GetPosts />
+        </div>
+      </>
+    );
+  },
+  save: () => {
+    // For dynamic blocks return null
+    return null;
+  },
+});
---
+++ src/inspector-control-tabs/inspector-control-tabs.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Code for laying out a more complex dynamic block.
+ * This used attributes saved from the Block Inspector.
+ */
+function myprefix_inspector_control_tabs_cb( $attributes, $content ) {
+
+  $recent_posts = wp_get_recent_posts( array(
+      'numberposts' => 5,
+      'post_status' => 'publish',
+  ) );
+
+  if ( count( $recent_posts ) === 0 ) {
+      return 'No posts';
+  }
+
+  $style = sprintf( 'text-decoration: %1$s; font-family: %2$s;',
+    isset( $attributes['underline'] ) ? 'underline' : 'none',
+    isset( $attributes['font'] ) ? esc_attr( $attributes['font'] ) : 'inherit'
+  );
+
+  $out = isset( $attributes['showTitle'] ) 
+    ? "<h2 style='$style'>{$attributes['title']}</h2>" 
+    :  '';
+
+  foreach ( $recent_posts as $a_post ) {
+    $out .= sprintf( '<p><a class="wp-block-my-plugin-latest-post" href="%1$s">%2$s</a></p>',
+      isset( $a_post['ID'] ) ? esc_url( get_permalink( $a_post['ID'] ) ) : '',
+      isset( $a_post['ID'] ) ? esc_html( get_the_title( $a_post['ID'] ) ) : ''
+  );
+  }
+
+  $extra_class = isset( $attributes['className'] ) ? " {$attributes['className']}" : '';
+
+  return "<div class='my-dynamic-block{$extra_class}'>$out</div>";
+}
+
+add_action( 'init', 'myprefix_inspector_control_tabs' );
+
+function myprefix_inspector_control_tabs() {
+
+  if ( ! function_exists( 'register_block_type' ) ) {
+      // Gutenberg is not active.
+      return;
+  }
+
+  register_block_type( __DIR__, array(
+    'render_callback' => 'myprefix_inspector_control_tabs_cb',  
+  ) );
+}
+
+
+/**
+ * Uncommenting to disable the Block Inspector control tabs for all blocks'.
+ * The inspector controls will display as they did pre-WP 6.2
+ */
+//add_filter('block_editor_settings_all', 'myprefix_disable_tabs_by_default');
+
+function myprefix_disable_tabs_by_default( $settings ) {
+  $settings['blockInspectorTabs'] = array( 'default' => false );
+  return $settings;
+}
+
+/**
+ * Uncomment this filter to disable the Block Inspector control tabs
+ * for this block.
+ */
+// add_filter('block_editor_settings_all', 'myprefix_disable_tabs_for_this_block');
+
+function myprefix_disable_tabs_for_this_block( $settings ) {
+  if ( !is_array($settings) ) {
+    return;
+  }
+
+  $current_tab_settings = array_key_exists('blockInspectorTabs', $settings) 
+    ? $settings['blockInspectorTabs'] 
+    : array();
+  $settings['blockInspectorTabs'] = array_merge(
+    $current_tab_settings, 
+    array('myprefix/inspector-control-tabs' => false)
+  );
+  return $settings;
+}
