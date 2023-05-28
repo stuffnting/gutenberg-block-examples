05242b6f5bf7b5852186356de265bac4431616cd
stuffnting <richard@stuffnting.com>
2023-05-28 10:30:54 +0100

    Remove root README.md from webpack.confog.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 // const { merge } = require("webpack-merge");
-const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // Get the build list from the JSON file. `require` converts JSON to an object.
-const BUILD_LIST_DATA = require("./src/build-list.json");
+const BUILD_LIST_DATA = require('./src/build-list.json');
 
 /**
  * Make an array of the block names that need building.
@@ -57,7 +57,7 @@ const buildListObj = buildListArray.length
  * @returns {boolean} - Whether to copy the file to the build folder.
  */
 const filterCB = (absoluteSourcePath) => {
-  const pathArray = absoluteSourcePath.split("/");
+  const pathArray = absoluteSourcePath.split('/');
   const fileDirectory = pathArray.slice(-2, -1).join();
 
   if (buildListArray.includes(fileDirectory)) {
@@ -74,8 +74,8 @@ const filterCB = (absoluteSourcePath) => {
  *                      Either two elements for example-name.ext, or three elements for example-name.index.ext
  */
 const processFilename = (absoluteFilename) => {
-  const directory = absoluteFilename.split("/").slice(-2, -1).join();
-  const filenameArray = absoluteFilename.split("/").slice(-1).join().split(".");
+  const directory = absoluteFilename.split('/').slice(-2, -1).join();
+  const filenameArray = absoluteFilename.split('/').slice(-1).join().split('.');
 
   return { directory, filenameArray };
 };
@@ -84,8 +84,8 @@ const processFilename = (absoluteFilename) => {
  * Define the more complex copy-webpack-plugin patterns here.
  */
 const phpPattern = {
-  context: "src",
-  from: "*/*.php",
+  context: 'src',
+  from: '*/*.php',
   to({ absoluteFilename }) {
     const { directory, filenameArray } = processFilename(absoluteFilename);
     // ./src/example-name/example-name.php becomes index.php
@@ -99,22 +99,22 @@ const phpPattern = {
 };
 
 const cssPattern = {
-  context: "src",
-  from: "*/*.css",
-  to: "./[path]/style.css",
+  context: 'src',
+  from: '*/*.css',
+  to: './[path]/style.css',
   filter: filterCB,
   noErrorOnMissing: true,
 };
 
 const jsonPattern = {
-  context: "src",
-  from: "*/*.json",
+  context: 'src',
+  from: '*/*.json',
   to({ absoluteFilename }) {
     const { directory, filenameArray } = processFilename(absoluteFilename);
     // ./src/example-name/example-name.block.js becomes block.json
     // All other JSON files get copied without a name change
     return filenameArray.length === 3 &&
-      [directory, "block", "json"].every((el, i) => el === filenameArray[i])
+      [directory, 'block', 'json'].every((el, i) => el === filenameArray[i])
       ? `./${directory}/block.json`
       : `./${directory}/[name].json`;
   },
@@ -132,9 +132,9 @@ const extraPlugins = [
   new CopyPlugin({
     patterns: [
       // Single files
-      { from: "README.md", to: "./" },
-      { context: "src", from: "plugin.php", to: "./" },
-      { context: "src", from: "build-list.json", to: "./" },
+      //{ from: "README.md", to: "./" },
+      { context: 'src', from: 'plugin.php', to: './' },
+      { context: 'src', from: 'build-list.json', to: './' },
       // Patterns
       phpPattern,
       cssPattern,
@@ -147,11 +147,9 @@ const extraPlugins = [
  * Change the output folder for the MiniCssExtractPlugin in @WP/scripts config.
  * If new MiniCssExtractPlugin is used, two instances exist and 2 copies of css files are made.
  */
-const editMiniCss = defaultConfig.plugins.filter(
-  (el) => el instanceof MiniCssExtractPlugin
-)[0]; // Filter will return an array with one element
+const editMiniCss = defaultConfig.plugins.filter((el) => el instanceof MiniCssExtractPlugin)[0]; // Filter will return an array with one element
 
-editMiniCss.options.filename = "[name]/style.css";
+editMiniCss.options.filename = '[name]/style.css';
 
 // Merge plugins
 const plugins = defaultConfig.plugins.concat(extraPlugins, editMiniCss);
@@ -164,8 +162,8 @@ module.exports = {
   plugins,
   entry: buildListObj,
   output: {
-    path: path.join(__dirname, "/start"),
-    filename: "[name]/index.js",
+    path: path.join(__dirname, '/start'),
+    filename: '[name]/index.js',
     clean: true,
   },
 };
