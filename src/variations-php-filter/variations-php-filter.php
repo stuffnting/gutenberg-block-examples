5b6fccf633b0fc53825aab62379817aa6344aa30
stuffnting <richard@stuffnting.com>
2024-04-09 12:14:31 +0100

    Add variations-php-filter

---
+++ src/variations-php-filter/variations-php-filter.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Use filter
+ */ 
+
+ add_filter( 'get_block_type_variations', 'myprefix_add__core_block_type_variations', 10, 2 );
+
+ function myprefix_add__core_block_type_variations( $variations, $block_type ) {
+ 
+  // This variation chooses 'Show media on right`, the default block is on left.
+  if ( 'core/media-text' === $block_type->name ) {
+    $variations[] = array(
+      'name'       => 'myprefix-media-right',
+      'title'      => __( 'Media & Text: Right', 'myprefix' ),
+      'scope'      => 'transformer',
+      'isActive'   => array(
+        'mediaPosition' 
+      ),
+      'attributes' => array(
+        'mediaPosition' => 'right'
+      )
+    );
+  }
+
+   return $variations;
+ }
