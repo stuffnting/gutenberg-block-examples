536eaedbe47878d91d5398c4d4a6f9bcba3b83eb
stuffnting <richard@stuffnting.com>
2024-04-10 16:55:00 +0100

    fix variations-php-filter

--- src/variations-php-filter/variations-php-filter.php
+++ src/variations-php-filter/variations-php-filter.php
@@ -4,9 +4,9 @@
  * Use filter
  */ 
 
- add_filter( 'get_block_type_variations', 'myprefix_add__core_block_type_variations', 10, 2 );
+ add_filter( 'get_block_type_variations', 'myprefix_add_core_block_type_variations', 10, 2 );
 
- function myprefix_add__core_block_type_variations( $variations, $block_type ) {
+ function myprefix_add_core_block_type_variations( $variations, $block_type ) {
  
   // This variation chooses 'Show media on right`, the default block is on left.
   if ( 'core/media-text' === $block_type->name ) {
